ba55150dedb9192218c0115c0374f362ff63f372
haikal-266 <haikalrizky266@gmail.com>
2024-08-13 16:09:39 +0700

    menampilkan data belanjaan baru, fitur delete, sort, checklist dan clear

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -4,20 +4,20 @@ import swal from 'sweetalert';
 const barangList = [
     {    
         id: 1,
-        nama: 'kopi',
-        qty: 23,
+        name: 'Kopi Kapal Api',
+        qty: 10,
         checked: false
     },
     {    
         id: 2,
-        nama: 'bacangi',
+        name: 'Rinso Bubuk',
         qty: 3,
         checked: true
     },
     {    
         id: 3,
-        nama: 'rinso',
-        qty: 8,
+        name: 'Kecap Bango ',
+        qty: 2,
         checked: false
     },
 ];
@@ -26,13 +26,17 @@ const barangList = [
 export function Header() {
   return (
     <header className="header">
-      <h2>Keranjang Belanjaan Anda</h2>
+      <h2>
+        Keranjang Belanjaan Rutin Anda
+        <br />
+        &#128221;
+      </h2>
     </header>
   );
 }
 
 
-export function Form() {
+export function Form({ onAddItem }) {
     const [name, setName] = useState('');
     const [qty, setQty] = useState(1);
 
@@ -52,14 +56,9 @@ export function Form() {
         return;
       }
     
-      const newItem = {name, qty, checked : false, id : Date.now()};
-      
-      swal({
-        title: "Good job!",
-        text: newItem.name + " Berhasil masuk keranjang",
-        icon: "success",
-        button: "Aww yiss!",
-      });
+      const newItem = {qty, name, checked : false, id : Date.now()};
+
+      onAddItem(newItem);
      
     }
   
@@ -83,24 +82,39 @@ export function Form() {
 
 
   
-export function ListBelanja() {
+export function ListBelanja({ items, onRemoveItem, onToogleItem, onClearItem }) {
+
+  const [sort, setSort] = useState('input');
+
+  let sortedItems = [...items];
+  if (sort === 'name') {
+    sortedItems.sort((a, b) => a.name.localeCompare(b.name));
+  } else if (sort === 'checked') {
+    sortedItems.sort((a, b) => Number(a.checked) - Number(b.checked));
+  }
+
   return (
     <>
       <div className="list">
         <ul>
-          {barangList.map((barang) => (
-            <BarangItem dataBarang={barang} key={barang.id} />
+          {sortedItems.map((barang) => (
+            <BarangItem 
+              dataBarang={barang} 
+              key={barang.id} 
+              onRemoveItem={onRemoveItem}
+              onToogleItem={onToogleItem}
+            />
           ))}
         </ul>
       </div>
 
       <div className="actions">
-        <select>
+        <select value={sort} onChange={(e) => setSort(e.target.value)}>
           <option value="input">Urutkan berdasarkan urutan input</option>
           <option value="name">Urutkan berdasarkan nama barang</option>
           <option value="checked">Urutkan berdasarkan ceklis</option>
         </select>
-        <button>Bersihkan Daftar</button>
+        <button onClick={onClearItem}>Bersihkan Daftar</button>
       </div>
     </>
   );
@@ -114,27 +128,48 @@ export function Footer() {
   );
 }
 
-function BarangItem({ dataBarang }) {
+function BarangItem({ dataBarang, onRemoveItem, onToogleItem }) {
     return (
       <li>
-        <input type="checkbox" defaultChecked={dataBarang.checked} />
+        <input type="checkbox" defaultChecked={dataBarang.checked} onChange={() => onToogleItem(dataBarang.id)} />
         <span style={{ textDecoration: dataBarang.checked ? 'line-through' : 'none' }}>
-          {dataBarang.qty} {dataBarang.nama}
-        </span>
-        <button>&times;</button>
+          {dataBarang.qty} {dataBarang.name}
+        </span>   
+        <button onClick={() => onRemoveItem(dataBarang.id)}>X</button>
       </li>
     );
-  }
-  
+  } 
 
-export default function Isi() {
+export default function App() {
+
+  const [items, setItems] = useState(barangList);
+
+  function handleAddItem(item) {
+    setItems([...items, item]);
+  };
+
+  function handleRemoveItem(id) {
+    setItems(items.filter(item => item.id !== id));
+
+  };
+
+  function handleToggleItem (id) {
+    setItems((items) => items.map((item) => item.id === id ? {...item, checked: !item.checked} : item));
+  }
+
+  function handleClearList() {
+    setItems([ ]);
+  }
+
+  
   return (
-    <div>
+    <div className="app">
       <Header />
-      <Form />
-      <ListBelanja />
+      <div className="main-content">
+        <Form onAddItem={handleAddItem} />
+        <ListBelanja items={items} onRemoveItem={handleRemoveItem} onToogleItem={handleToggleItem} onClearItem={handleClearList} />
+      </div>
       <Footer />
-      
     </div>
   );
 }
